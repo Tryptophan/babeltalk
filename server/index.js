1f2d6a3af772811a506f975186a3c405efa8e19c
Jacob Greenway <jagreenway12@gmail.com>
2018-10-13 09:21:51 -0500

    server events and logging

--- server/index.js
+++ server/index.js
@@ -1,7 +1,37 @@
 const io = require('socket.io')();
 
+// Array of users to hold state
+let users = [];
+
 io.on('connection', (client) => {
-  // TODO
+
+  // Send array of users to the client
+  client.on('users', () => {
+    io.to(client.id).emit(users);
+  });
+
+  // Send chat message
+  client.on('chat', chat => {
+    console.log(chat);
+  });
+
+  // Call another user
+  client.on('call', call => {
+    console.log(call);
+  });
+
+  /* WebRTC signalling */
+  
+  // Got offer from peer
+  client.on('offer', offer => {
+    console.log(offer);
+  });
+
+  // Got answer from peer
+  client.on('answer', answer => {
+    console.log(answer);
+  });
 });
 
 io.listen(3001);
+console.log('Listening on port 3001.');
