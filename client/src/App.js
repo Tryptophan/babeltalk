eabadb95702a6bcdd6508427f5140fa46473f1d3
briantoe <bht170030@utdallas.edu>
2018-10-12 23:01:58 -0500

    created general front end layout. removed logo.svg

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App'>
+        <div className='videoBox'>
+          video goes here
+        </div>
+        <div className='chatContainer'>
+          <ul id='chatLog'>{'chat log'} </ul>
+          <input onKeyPress={this.onKeyPress} type='text' placeholder='type here' id='textBox' />
+          <button id='sendBtn'>send</button>
+        </div>
+        <div className='userBar'>
+          <ul id='userList'>{'user list'}</ul>
+        </div>
+
+
       </div>
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
