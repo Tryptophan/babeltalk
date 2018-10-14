f0a06832586e3103dd207e8b582b693ee7358525
briantoe <bht170030@utdallas.edu>
2018-10-14 06:08:09 -0500

    added some support for Splashscreen.js in App.js

--- client/src/components/Splashscreen.js
+++ client/src/components/Splashscreen.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 
 export class Splashscreen extends Component {
 
+<<<<<<< HEAD
     constructor() {
       super();
     }
@@ -17,3 +18,47 @@ export class Splashscreen extends Component {
         );
     }
   }
+=======
+  constructor() {
+
+    super();
+    this.state = {
+      userName: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className='Splash'>
+        <label>BabelTalk - Real-time p2p translation communication platform</label>
+        <label>What is your name?</label>
+        <input type='text' onKeyPress={this.onKeyPress} ref={el => { this.input = el }} placeholder="Type here!" id='userNameBox' />
+        <button onClick={this.sendClicked}>Send</button>
+
+      </div>
+
+    );
+  }
+
+  onKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      console.log("username inputted");
+      let text = event.target.value;
+      this.setState({
+        userName: text
+      });
+    }
+
+  }
+  sendClicked = () => {
+    console.log("username inputted");
+    let text = this.input.value;
+    this.setState({
+      userName: text
+    });
+
+  }
+
+}
+
+>>>>>>> master
